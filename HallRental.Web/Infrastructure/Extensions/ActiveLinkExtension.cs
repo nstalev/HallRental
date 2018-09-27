86a2e5447585884b1de1e958becf91dc7818dac3
nstalev <stalev_a@abv.bg>
2018-09-27 16:48:28 +0300

    ActiveLink - added

---
+++ HallRental.Web/Infrastructure/Extensions/ActiveLinkExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace HallRental.Web.Infrastructure.Extensions
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Routing;
+
+    public static class ActiveLinkExtension
+    {
+        public static string IsLinkActive(this IUrlHelper url, string controller, string action)
+        {
+            var controller1 = url.ActionContext.RouteData.Values["controller"].ToString();
+            var action11 = url.ActionContext.RouteData.Values["action"].ToString();
+
+            if (url.ActionContext.RouteData.Values["controller"].ToString() == controller &&
+                url.ActionContext.RouteData.Values["action"].ToString() == action)
+            {
+                return "active";
+            }
+
+            return "";
+        }
+    }
+}
