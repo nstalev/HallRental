2c253a589b44b03fbc0a1cb5ff2a2181ed636a8a
nstalev <stalev_a@abv.bg>
2018-10-09 13:52:33 +0300

    bootstrap design

--- HallRental.Web/Models/EventsModel/SummaryAndPersonalInfoModel.cs
+++ HallRental.Web/Models/EventsModel/SummaryAndPersonalInfoModel.cs
@@ -19,6 +19,8 @@ namespace HallRental.Web.Models.EventsModel
         public RentTimeEnum RentTime { get; set; }
 
         [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
         public string EventTitle { get; set; }
 
 
